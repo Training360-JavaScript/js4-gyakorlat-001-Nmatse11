b457f2820377fd06c093ba7f525f298b847475c7
Nmatse <atseniko@gmail.com>
2021-11-17 17:02:38 +0100

    ready

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,8 @@
-
-
-
-
-
-
-// export default sorterPromise;
+const  sorterPromise = (array) => new Promise((resolve, reject) => {
+  if (array.every(item => typeof item === 'string')) {
+    resolve(array.sort())
+  } else {
+    reject('Error: Not all elements are string type!')
+  }
+});
+export default sorterPromise;
